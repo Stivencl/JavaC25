f48a3baa2c27cb060e4a467912a4876d696c867b
Stivencl <stevfoun@gmail.com>
2024-06-12 12:40:41 -0500

    Actividad 03 palindromo

--- src/main/java/Actividades/Actividad03.java
+++ src/main/java/Actividades/Actividad03.java
@@ -8,17 +8,36 @@ y de atrás hacia adelante (ignorando espacios, puntuación y mayúsculas).
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Actividad03 {
     public static void main(String[] args) {
 
-        String palabra = "arenera";
-        ArrayList<Character> list = new ArrayList<Character>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite la palabra o frase deseada señor usuario:");
+        String palabra =  scanner.nextLine();
+        palabra = palabra.toLowerCase();
+        palabra = palabra.replace(" ","");
         char[] caracterArray = palabra.toCharArray();
-        for (char a : caracterArray)
-            list.add(a);
 
-        
+        int izq = 0;
+        int der = caracterArray.length-1;
+
+        for (int i = izq; i < der; i++){
+
+            if (caracterArray[izq] == caracterArray[der]){
+                der --;
+                izq++;
+
+            }else {
+                System.out.println("La paralbra o frase digitada no es un palindromo");
+                break;
+            }
+        }
+       if (izq == der){
+            System.out.println("La palabra o frase digitada es un palindromo");
+        }
+        scanner.close();
 
 
     }//fin del metodo main
